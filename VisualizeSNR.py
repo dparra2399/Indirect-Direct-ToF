2bcd9a703d16a4651afabe477be8917cfea96ffd
David Parra <dparra2399@gmail.com>
2023-09-23 23:09:05 -0500

    updated tof

--- VisualizeSNR.py
+++ VisualizeSNR.py
@@ -23,26 +23,25 @@ import FileUtils
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-mae = np.load('./data/ntbins_1024_k_4_coding_coscos_monte_50_exp_18.npz')
+mae = np.load('./data/ntbins_1024_k_4_coding_coscos_monte_1000_exp_21.npz')
 
 pAveSourceList = mae['pAveSourceList']
 pAveAmbientList = mae['pAveAmbientList']
 mae_idtof = mae['SNR_IDTOF']
 mae_itof = mae['SNR_ITOF']
 
-arr = [0]
-pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=1)
-pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=1)
-mae_idtof = np.delete(mae_idtof, obj=arr, axis=1)
-mae_itof = np.delete(mae_itof, obj=arr, axis=1)
-
+#arr = [0]
+#pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=1)
+#pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=1)
+#mae_idtof = np.delete(mae_idtof, obj=arr, axis=1)
+#mae_itof = np.delete(mae_itof, obj=arr, axis=1)
 
 #pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=0)
 #pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=0)
 #mae_idtof = np.delete(mae_idtof, obj=arr, axis=0)
 #mae_itof = np.delete(mae_itof, obj=arr, axis=0)
 
-diff = mae_idtof / mae_itof
+diff = mae_idtof - mae_itof
 ax.plot_surface(np.log10(pAveSourceList), np.log10(pAveAmbientList), mae_idtof, cmap=cm.coolwarm,linewidth=0, antialiased=False)
 ax.plot_surface(np.log10(pAveSourceList), np.log10(pAveAmbientList), mae_itof, cmap=cm.autumn, linewidth=0, antialiased=False)
 
