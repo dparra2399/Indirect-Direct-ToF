45bd25b87c0e860c24ca1abe4d21766ab858386c
dparra2399 <dpara2399@gmail.com>
2023-09-24 21:39:29 -0500

    driect commit

--- VisualizeSNR.py
+++ VisualizeSNR.py
@@ -23,29 +23,37 @@ import FileUtils
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-mae = np.load('./data/ntbins_1024_k_4_coding_coscos_monte_1000_exp_19.npz')
+mae = np.load('./data/ntbins_1024_k_4_coding_coscos_monte_1000_exp_20.npz')
+direct = np.load('./data/direct_ntbins_1024_monte_1000_exp_0.npz')
 
 pAveSourceList = mae['pAveSourceList']
 pAveAmbientList = mae['pAveAmbientList']
 mae_idtof = mae['SNR_IDTOF']
 mae_itof = mae['SNR_ITOF']
 
-#arr = [0]
-#pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=1)
-#pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=1)
-#mae_idtof = np.delete(mae_idtof, obj=arr, axis=1)
-#mae_itof = np.delete(mae_itof, obj=arr, axis=1)
+#pAveSourceList = direct['pAveSourceList']
+#pAveAmbientList = direct['pAveAmbientList']
+mae_dtof = direct['mae']
 
-#pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=0)
-#pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=0)
-#mae_idtof = np.delete(mae_idtof, obj=arr, axis=0)
-#mae_itof = np.delete(mae_itof, obj=arr, axis=0)
+arr = []
+pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=1)
+pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=1)
+mae_idtof = np.delete(mae_idtof, obj=arr, axis=1)
+mae_itof = np.delete(mae_itof, obj=arr, axis=1)
+mae_dtof = np.delete(mae_dtof, obj=arr, axis=1)
+
+pAveSourceList = np.delete(pAveSourceList, obj=arr, axis=0)
+pAveAmbientList = np.delete(pAveAmbientList, obj=arr, axis=0)
+mae_idtof = np.delete(mae_idtof, obj=arr, axis=0)
+mae_itof = np.delete(mae_itof, obj=arr, axis=0)
+mae_dtof = np.delete(mae_dtof, obj=arr, axis=0)
 
 diff = mae_idtof - mae_itof
 ax.plot_surface(np.log10(pAveSourceList), np.log10(pAveAmbientList), mae_idtof, cmap=cm.coolwarm,linewidth=0, antialiased=False)
 ax.plot_surface(np.log10(pAveSourceList), np.log10(pAveAmbientList), mae_itof, cmap=cm.autumn, linewidth=0, antialiased=False)
 
 #ax.plot_surface(np.log(pAveSourceList), np.log(pAveAmbientList), diff, cmap=cm.autumn,linewidth=0, antialiased=False)
+ax.plot_surface(np.log10(pAveSourceList), np.log10(pAveAmbientList), mae_dtof, cmap=cm.summer,linewidth=0, antialiased=False)
 
 ax.set_xlabel('pAve source')
 ax.set_ylabel('pAve ambient')
